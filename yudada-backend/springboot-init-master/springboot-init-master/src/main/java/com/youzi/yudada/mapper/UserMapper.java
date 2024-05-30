17e6e0ca23865d254eb763e3c84059d832cb0a8e
stevekuli <2966392054@qq.com>
2024-05-30 10:41:34 +0800

    答题应用平台后端

---
+++ yudada-backend/springboot-init-master/springboot-init-master/src/main/java/com/youzi/yudada/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.youzi.yudada.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.youzi.yudada.model.entity.User;
+
+/**
+ * 用户数据库操作
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
